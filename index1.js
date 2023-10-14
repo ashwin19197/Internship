ae29c70618e3ebbc0026597f5a79f1fd88fb4a7e
ashwin19197 <147700263+ashwin19197@users.noreply.github.com>
2023-10-14 12:15:33 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,68 @@
+//prompt("Enter the name");
+//console.log("this is console");
+//console.log(6+5);
+//document.write("<h1>Javascript</h1>");
+
+
+// var num1=5;
+// var num1=6;
+// num1=8
+// console.log(num1);
+
+// let num2=9
+// num2=11
+// console.log(num2);
+
+// const num3=10
+// console.log(num3);
+// console.log(3**2);
+// let x=5
+// x++
+// console.log(x);
+// x---
+// console.log(x);
+
+// let num1=5
+// let num2="5"
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+
+// function mul(n1,n2,n3,n4){
+//     return n1*n2*n3*n4
+// }
+
+// document.write(mul(4,5,6,7));
+// function greet(){
+//     alert("hello everyo")
+// }
+
+
+// let age=19
+// if(age>=18){
+//     document.write("elligble to vote")
+// }else{
+//     document.write(" not elligble to vote")
+// }
+
+
+// for(let n=10;n>=0;n--){
+//     console.log(n)
+// }
+
+
+
+
+// Dom-document object model
+// dom manipulation methods
+// documents.getElementById()
+// documents.getElementByClassName()
+// documents.getElementByTagName()
+// documents.querySelector()
+// documents.querySelectorsAll()
+
+
+// let ek=document.getElementById("head").innerHTML;
+// console.log(ek);
+
+let ele= document.querySelector("#head").innerHTML;
+console.log(ele);
